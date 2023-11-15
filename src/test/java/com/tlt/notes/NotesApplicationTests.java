4022cfca7d6200995d062bddbe8a3d601409162f
Virginie Garcin <virginie@exauceo.fr>
2023-11-15 11:45:27 +0400

    Create application.

---
+++ src/test/java/com/tlt/notes/NotesApplicationTests.java
@@ -0,0 +1,11 @@
+package com.tlt.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
